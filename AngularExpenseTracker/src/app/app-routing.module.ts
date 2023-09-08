f63d1c476e8a8abc999c1eec8c93fa88bd5aa7bf
mohammad eunus <eunusur.rahaman.cse@ulab.edu.bd>
2023-09-09 03:27:23 +0600

    route to expense comp

--- AngularExpenseTracker/src/app/app-routing.module.ts
+++ AngularExpenseTracker/src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
 import { CategoriesComponent } from './Components/categories/categories.component';
+import { ExpenseComponent } from './Components/expense/expense.component';
 
 const routes: Routes = [
   {
     path: 'category',
     component: CategoriesComponent, // Use the function to determine the component
+  },
+  {
+    path: 'expenses',
+    component: ExpenseComponent, // Use the function to determine the component
   },
 ];
 
