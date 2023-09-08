6469c1888cd46b9c507bd94d85d16b04ef40b53b
mohammad eunus <eunusur.rahaman.cse@ulab.edu.bd>
2023-09-09 01:23:52 +0600

    expense comp created

--- AngularExpenseTracker/src/app/app.module.ts
+++ AngularExpenseTracker/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
 import { CategoriesComponent } from './Components/categories/categories.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ExpenseComponent } from './components/expense/expense.component';
 
 @NgModule({
-  declarations: [AppComponent, NavBarComponent, CategoriesComponent],
+  declarations: [AppComponent, NavBarComponent, CategoriesComponent, ExpenseComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
