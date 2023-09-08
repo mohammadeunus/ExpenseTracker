23110b92ec4afd7d9b0f9e4db284277f5548c1ee
mohammad eunus <eunusur.rahaman.cse@ulab.edu.bd>
2023-09-09 03:06:33 +0600

    correction

--- AngularExpenseTracker/src/app/app.module.ts
+++ AngularExpenseTracker/src/app/app.module.ts
@@ -5,10 +5,15 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
 import { CategoriesComponent } from './Components/categories/categories.component';
 import { AppRoutingModule } from './app-routing.module';
-import { ExpenseComponent } from './components/expense/expense.component';
+import { ExpenseComponent } from './Components/expense/expense.component';
 
 @NgModule({
-  declarations: [AppComponent, NavBarComponent, CategoriesComponent, ExpenseComponent],
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    CategoriesComponent,
+    ExpenseComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
